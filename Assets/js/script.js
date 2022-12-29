432085e85f7580a825e0734b51bbefe766cd68a5
lindstdb <115056565+lindstdb@users.noreply.github.com>
2022-12-28 19:51:07 -0600

    added js basic functionality

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,148 +1,111 @@
-//Sets what questions will be asked
-const Questions =[{
-    id: 0,
-    q: "Commonly used data types DO NOT include:",
-    a: [{text:"Boolean", isCorrect: false },
-        {text:"Number", isCorrect: false},
-        {text:"Airplanes", isCorrect: true},
-        {text:"String", isCorrect: false}
-    ]
-},
-{
-    id:1,
-    q: "The condition in an if/else statement is enclosed within what?",
-    a: [{text:"Parenthesis", isCorrect: true, isSelected: false},
-        {text:"Curly Brackets", isCorrect: false},
-        {text:"Square Brackets", isCorrect: false},
-        {text:"Quotes", isCorrect: false}
-    ]
-},
-{
-    id:2,
-    q: "Arrays in JavaScript can be used to store...",
-    a: [{text:"Numbers and Strings", isCorrect: false},
-        {text:"Other Arrays", isCorrect: false},
-        {text:"Booleances", isCorrect: false},
-        {text:"All of the Above", isCorrect: true}
-    ]
-},
-{
-    id:3,
-    q: "String values must be enclosed within .... when being assigned to variables",
-    a: [{text:"Commas", isCorrect: false},
-        {text:"Curley Brackets", isCorrect: false},
-        {text:"Quotes", isCorrect: true},
-        {text:"Parenthesis", isCorrect: false}
-    ]
-},
-{
-    id:4,
-    q: "A useful tool used by developers to print content to the debugger",
-    a: [{text:"JavaScript", isCorrect: false},
-        {text:"Terminal/Bash", isCorrect: false},
-        {text:"Alerts", isCorrect: false},
-        {text:"Console.log", isCorrect: true}
-    ]
+var startQuizBtn = document.querySelector(".start-quiz-btn");
+var timer = document.querySelector("#timer");
+var question = document.querySelector(".question");
+var answer1 = document.querySelector(".answer1");
+var answer2 = document.querySelector(".answer2");
+var answer3 = document.querySelector(".answer3");
+var answer4 = document.querySelector(".answer4");
+var quizSection = document.querySelector(".quiz-section");
+var rightWrong = document.querySelector("#correct-or-incorrect");
+var initialsSection = document.querySelector(".initals-section");
+var submitButton = document.querySelector(".submit-btn");
+var highScoreSection = document.querySelector(".highscores-section");
+var viewScores = document.querySelector("#highscore");
+
+// Questions Array
+var question1 = ""
+var question2 = ""
+var question3 = ""
+var question4 = ""
+var question5 = ""
+var questionsArray = [question1, question2, question3, question4, question5]
+
+// Answers
+var quizChoice1 = {
+    choice1: "",
+    choice2: "",
+    choice3: "",
+    choice4: "",
 }
-]
-
-// Set the Start 
-var start = true;
-
-// Iterate
-function iterate(id) {
-
-    //Display Outcome section
-    var outcome = document.getElementsByClassName("outcome");
-    outcome[0].innerText = "";
-
-    //Gets the question 
-    const question = document.getElementById("question");
-
-    //Sets the questions text
-    question.innerText = Questions[id].q;
-
-    //Gets the answer options
-    const a1 = document.getElementById('a1');
-    const b2 = document.getElementById('b2');
-    const c3 = document.getElementById('c3');
-    const d4 = document.getElementById('d4');
-
-    // Gives the options text
-    a1.innerText = Questions[id].a[0].text;
-    b2.innerText = Questions[id].a[1].text;
-    c3.innerText = Questions[id].a[2].text;
-    d4.innerText = Questions[id].a[3].text;
-
-    //Applies True or False values to options
-    a1.value = Questions[id].a[0].isCorrect;
-    b2.value = Questions[id].a[1].isCorrect;
-    c3.value = Questions[id].a[2].isCorrect;
-    d4.value = Questions[id].a[3].isCorrect;
-
-
-    //Shows selection for different options
-    var selected = "";
-
-    a1.addEventListener("click",() => {
-        a1.style.backgroundColor = "goldenrod";
-        b2.style.backgroundColor = "lightblue";
-        c3.style.backgroundColor = "lightblue";
-        d4.style.backgroundColor = "lightblue";
-        selected = a1.value;
-    })
-
-    b2.addEventListener("click", () => {
-        a1.style.backgroundColor = "lightblue";
-        b2.style.backgroundColor = "goldenrod";
-        c3.style.backgroundColor = "lightblue";
-        d4.style.backgroundColor = "lightblue";
-        selected = b2.value; 
-    })
-
-    c3.addEventListener("click", () => {
-        a1.style.backgroundColor = "lightblue";
-        b2.style.backgroundColor = "lightblue";
-        c3.style.backgroundColor = "goldenrod";
-        d4.style.backgroundColor = "lightblue";
-        selected = c3.value;   
-    })
-
-    d4.addEventListener("click", () => {
-        a1.style.backgroundColor = "lightblue";
-        b2.style.backgroundColor = "lightblue";
-        c3.style.backgroundColor = "lightblue";
-        d4.style.backgroundColor = "goldenrod";
-        selected = d4.value; 
-    })
-
-    //Check Answer capabilities
-    const checkAnswer = document.getElementsByClassName("checkAnswer");
-
-    checkAnswer[0].addEventListener("click", () => {
-        if (selected == "true") {
-            checkAnswer[0].innerHTML = "True";
-            checkAnswer[0].style.color = "green";
-        } else{
-            checkAnswer[0].innerHTML = "False";
-            checkAnswer[0].style.color = "red";
-        }
-    })
+var quizChoice2 = {
+    choice1: "",
+    choice2: "",
+    choice3: "",
+    choice4: "",
+}
+var quizChoice3 = {
+    choice1: "",
+    choice2: "",
+    choice3: "",
+    choice4: "",
+}
+var quizChoice4 = {
+    choice1: "",
+    choice2: "",
+    choice3: "",
+    choice4: "",
+}
+var quizChoice5 = {
+    choice1: "",
+    choice2: "",
+    choice3: "",
+    choice4: "",
 }
 
-if (start) {
-    iterate("0");
-}
+// Answer Array
+var answersArray = [quizChoice1, quizChoice2, quizChoice3, quizChoice4, quizChoice5]
 
-//Next button functionality
-const next = document.getElementsByClassName('next')[0];
-var id = 0;
+// Correct Answers
+var correct1 = quizChoice1.choice3;
+var correct2 = quizChoice2.choice2;
+var correct3 = quizChoice3.choice1;
+var correct4 = quizChoice4.choice2;
+var correct5 = quizChoice5.choice4;
+var correctArray = [correct1, correct2, correct3, correct4, correct5]
 
-next.addEventListener("click", () => {
-    start = false;
-    if (id<5) {
-        id++;
-        iterate(id);
-        console.log(id);
-    }
+// Start Quiz Button 
+startQuizBtn.addEventListener("click", startTimer)
+
+// Hide Intro Page
+startQuizBtn.addEventListener("click", function(){
+    document.querySelector(".eagle").getElementsByClassName.display = "none";
+    quizSection.getElementsByClassName.display="block";
 })
+
+// Show First Question
+startQuizBtn.addEventListener("click", nextQuestion)
+
+
+var correctIndex = 0;
+function nextQuestion(){
+    if(correctIndex === questionsArray.length - 1) {
+        setTimeout(funtion(){quizSection.style.display = "none";
+        initialsSection.style.display = "inline";
+    }, 500);
+    setTimeout(function(){clearInterval(timeInterval)},500);
+    }else {
+        question.textContent = questionsArray[correctIndex];
+        answer1.textContent = answersArray[correctIndex].choice1;
+        answer2.textContent = answersArray[correctIndex].choice2;
+        answer3.textContent = answersArray[correctIndex].choice3;
+        answer4.textContent = answersArray[correctIndex].choice4;
+    }
+}
+
+var secondsLeft = 71;
+var timerInterval;
+function startTimer(){
+    timerInterval = setInterval(function() {
+        secondsLeft --;
+        timer.textContent = "Time: " + secondsLeft + " seconds";
+        
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+            quizSection.style.display = "inline";
+        }
+    }, 1000);
+    return timerInterval;
+}
+
+
+
